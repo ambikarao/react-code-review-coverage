6d83c85fa8c353e4f58b85faf574756a693ae579
Swatantra Mukherjee <swatantraguccho@gmail.com>
2025-10-10 10:47:42 +0530

    commit

--- src/pages/Address.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import Address from "./Address";
-
-test("renders Address component", () => {
-  render(<Address />);
-  expect(screen.getByTestId("address-component")).toBeInTheDocument();
-});
