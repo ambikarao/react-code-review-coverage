c72b9124388f7267da1c9bc80ebfcccdd2db312a
Ambika <balambika.pantala@ibm.com>
2025-11-05 13:04:07 +0530

    Issues:   1. Missing type annotations
      2. Unused imports
    
      3. Missing key prop in Routes
    
      4. console.log statements
    
    Optimizations:
      1. Untitled
    
      2. Untitled
    
      3. Untitled
    
      4. Untitled

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,37 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { AppProvider } from "./AppContext";
-import { NotificationProvider } from "./components";
-import { Header } from "./components";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
-import ProductList from "./pages/ProductList";
-import Cart from "./pages/Cart";
-import Wishlist from "./pages/Wishlist";
-import Checkout from "./pages/Checkout";
-import Orders from "./pages/Orders";
-import Profile from "./pages/Profile";
-import Contact from "./pages/Contact";
-import "./App.scss";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppProvider } from './AppContext';
+import { NotificationProvider } from './components';
+import { Header } from './components';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import ProductList from './pages/ProductList';
+import Cart from './pages/Cart';
+import Wishlist from './pages/Wishlist';
+import Checkout from './pages/Checkout';
+import Orders from './pages/Orders';
+import Profile from './pages/Profile';
+import Contact from './pages/Contact';
+import './App.scss';
 
-const App: React.FC = () => {
+const App: React.FC = React.memo(() => {
   return (
     <AppProvider>
       <NotificationProvider>
         <BrowserRouter>
-          <div className="app-shell">
+          <div className='app-shell'>
             <Header />
-            <main className="app-main">
+            <main className='app-main'>
               <Routes>
-                <Route path="/" element={<ProductList />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/wishlist" element={<Wishlist />} />
-                <Route path="/checkout" element={<Checkout />} />
-                <Route path="/orders" element={<Orders />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/contact" element={<Contact />} />
+                <Route key='product-list' path='/' element={<ProductList />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/signup' element={<SignUp />} />
+                <Route path='/cart' element={<Cart />} />
+                <Route path='/wishlist' element={<Wishlist />} />
+                <Route path='/checkout' element={<Checkout />} />
+                <Route path='/orders' element={<Orders />} />
+                <Route path='/profile' element={<Profile />} />
+                <Route path='/contact' element={<Contact />} />
               </Routes>
             </main>
           </div>
@@ -39,8 +39,6 @@ const App: React.FC = () => {
       </NotificationProvider>
     </AppProvider>
   );
-};
+});
 
 export default App;
-
-
