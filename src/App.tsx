cd467a574737e960313cf441ce1dd8d1f8a7fab0
EC2 Default User <ec2-user@ip-172-31-20-209.ap-southeast-2.compute.internal>
2025-11-07 07:51:21 +0000

    Issues:   1. Missing key prop in Routes
      2. Missing type annotations for props
    
      3. console.log in production code
    
    Optimizations:
      1. Untitled
    
      2. Untitled
    
      3. Untitled
    
      4. Untitled
    
      5. Untitled
    
      6. Untitled

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useMemo, useCallback } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AppProvider } from "./AppContext";
-import { NotificationProvider } from "./components";
-import { Header } from "./components";
+import { NotificationProvider, Header } from "./components";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import ProductList from "./pages/ProductList";
@@ -14,7 +13,19 @@ import Profile from "./pages/Profile";
 import Contact from "./pages/Contact";
 import "./App.scss";
 
-const App: React.FC = () => {
+const App: React.FC = React.memo(() => {
+  const routes = useMemo(() => [
+    <Route key="product-list" path="/" element={<ProductList />} />, 
+    <Route path="/login" element={<Login />} />, 
+    <Route path="/signup" element={<SignUp />} />, 
+    <Route path="/cart" element={<Cart />} />, 
+    <Route path="/wishlist" element={<Wishlist />} />, 
+    <Route path="/checkout" element={<Checkout />} />, 
+    <Route path="/orders" element={<Orders />} />, 
+    <Route path="/profile" element={<Profile />} />, 
+    <Route path="/contact" element={<Contact />} />
+  ], []);
+
   return (
     <AppProvider>
       <NotificationProvider>
@@ -22,25 +33,13 @@ const App: React.FC = () => {
           <div className="app-shell">
             <Header />
             <main className="app-main">
-              <Routes>
-                <Route path="/" element={<ProductList />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/wishlist" element={<Wishlist />} />
-                <Route path="/checkout" element={<Checkout />} />
-                <Route path="/orders" element={<Orders />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/contact" element={<Contact />} />
-              </Routes>
+              <Routes>{routes}</Routes>
             </main>
           </div>
         </BrowserRouter>
       </NotificationProvider>
     </AppProvider>
   );
-};
+});
 
 export default App;
-
-
